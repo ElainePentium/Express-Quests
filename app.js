8b093c8654326e0c29af2eefe9a2221cbdb04c8a
Stéphanie Gentil <gentil.stephanie@gmail.com>
2023-06-26 10:31:28 +0200

    challenge auth

--- app.js
+++ app.js
@@ -1,21 +1,28 @@
 require("dotenv").config();
 const express = require("express");
 
-
 const app = express();
 
 const port = process.env.APP_PORT ?? 5000;
 
+
 const welcome = (req, res) => {
   res.send("Welcome to my favourite movie list");
 };
-
 app.get("/", welcome);
 
-const movieHandlers = require("./movieHandlers");
 
+const movieHandlers = require("./movieHandlers");
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
+
+
+const { hashPassword } = require("./auth.js");
+app.post("/api/users", hashPassword, userHandlers.postUser);
+app.put("/api/users/:id", hashPassword, userHandlers.updateUser);
+
+
+
 
 app.listen(port, (err) => {
   if (err) {
